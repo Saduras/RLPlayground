0ce955e325775b59074acc4a4922221c96a33b68
David Speck <david@saduras.de>
2018-07-26 12:50:03 +0200

    EndlessRunner: Import project

---
+++ RL-unity-env/Assets/EndlessRunner/Scripts/EndlessRunnerAcademy.cs
@@ -0,0 +1,33 @@
+﻿using MLAgents;
+using UnityEngine;
+
+public class EndlessRunnerAcademy : Academy {
+
+    public float obstacleSpeed = 3f;
+    public Transform obstacle;
+
+    private Vector3 obstacleStartPos;
+    private float randomRange = 2f;
+
+	public override void InitializeAcademy()
+	{
+        obstacleStartPos = obstacle.position;
+	}
+
+	public override void AcademyStep()
+	{
+        obstacle.Translate(Vector3.left * obstacleSpeed * Time.fixedDeltaTime);
+        if (obstacle.position.x < 0)
+            PlaceObstacle();
+    }
+
+	public override void AcademyReset()
+	{
+        PlaceObstacle();
+    }
+
+    private void PlaceObstacle()
+    {
+        obstacle.position = obstacleStartPos + new Vector3(Random.Range(-randomRange, randomRange), 0f, 0f);
+    }
+}
