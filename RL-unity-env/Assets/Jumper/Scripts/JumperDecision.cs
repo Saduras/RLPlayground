363485c2c0e46d415e61e6a1980abfaafe2699c2
David Speck <david@saduras.de>
2018-07-28 13:17:51 +0200

    Jumper: Implement heuristic decision

---
+++ RL-unity-env/Assets/Jumper/Scripts/JumperDecision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MLAgents;
+using UnityEngine;
+
+public class JumperDecision : MonoBehaviour, Decision
+{
+    public float[] Decide(List<float> vectorObs, List<Texture2D> visualObs, float reward, bool done, List<float> memory)
+    {
+        var actions = new float[1];
+        var obstacleDistance = vectorObs[0];
+        actions[0] = obstacleDistance < 2.5f ? 1f : 0f;
+        return actions;
+    }
+
+    public List<float> MakeMemory(List<float> vectorObs, List<Texture2D> visualObs, float reward, bool done, List<float> memory)
+    {
+        return new List<float>();
+    }
+}
